02019120bdba68b7feb1b73d9b3b580cda487fbb
Bin-baz Akilan <binbaz.akilan@outlook.com>
2023-07-28 23:42:21 +0800

    added the 404 page

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+def handling_404(request, exception):
+   hide_navbar = True
+   return render(request, 'pages/404.html', {'hide_navbar': hide_navbar})
+
 def index(request):
     return render(request, 'index.html')
 
